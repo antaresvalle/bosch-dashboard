6909f328e68ebcdcc862a16da3af3a5890b4a69a
Zetnavrek <vykovi@gmail.com>
2018-09-11 13:47:54 -0500

    Responsive board

--- src/App.js
+++ src/App.js
@@ -110,7 +110,7 @@ class App extends Component {
         <main>
         <div className="sensors container">
         <div className="row">
-          <div className="temperature sensor col-3">
+          <div className="temperature sensor col-sm-12 col-md-3 col-lg-3">
             <h2 className="col-12">Temperature</h2>
             <span className="value col-12">{this.state.data.temperature}℃</span>
             <div className="notification-sensor col-12">
@@ -119,7 +119,7 @@ class App extends Component {
               <span className="col-12">Status</span>
             </div>
           </div>
-          <div className="humidity sensor col-3">
+          <div className="humidity sensor col-sm-12 col-md-3 col-lg-3">
             <h2 className="col-12">Humidity</h2>
             <span className="value col-12">{this.state.data.humidity}%</span>
             <div className="notification-sensor col-12">
@@ -127,7 +127,7 @@ class App extends Component {
               <span className="col-12">Status</span>
             </div>
           </div>
-          <div className="noise sensor col-3">
+          <div className="noise sensor col-sm-12 col-md-3 col-lg-3">
           <h2 className="col-12">Noise</h2>
           <span className="value col-12">{this.state.data.noise}db</span>
           <div className="notification-sensor col-12">
@@ -135,11 +135,11 @@ class App extends Component {
               <span className="col-12">Status</span>
             </div>
           </div>
-          <div className="luminosity sensor col-3">
+          <div className="luminosity sensor col-sm-12 col-md-3 col-lg-3">
           <h2 className="col-12">Luminosity</h2>
           <span className="value col-12">{this.state.data.illumination}xl</span>
           <div className="notification-sensor col-12">
-          <i className="fas fa-exclamation notification-icon col-12"></i>
+          <i className="fas fa-exclamation notification-icon"></i>
               <span className="col-12">Status</span>
             </div>
           </div>
@@ -149,40 +149,40 @@ class App extends Component {
         <div className="line"></div>
       </main>
         <footer>
-          <div class="container final">
-            <div class="row">
-              <div class="col">
-                <div class="iconos">
+          <div className="container final">
+            <div className="row">
+              <div className="col">
+                <div className="iconos">
                   <h6>Stay up to date</h6>
                   <a href=""><i id="facebook" class="fab fa-facebook-f"></i></a>
-                  <a href=""><i class="fab fa-twitter"></i></a>
-                  <a href=""><i class="fab fa-youtube"></i></a>
-                  <a href=""><i class="fab fa-linkedin-in"></i></a>
+                  <a href=""><i className="fab fa-twitter"></i></a>
+                  <a href=""><i className="fab fa-youtube"></i></a>
+                  <a href=""><i className="fab fa-linkedin-in"></i></a>
                 </div>
               </div>
-              <div class="col">
-                <div class="touch">
+              <div className="col">
+                <div className="touch">
                   <h6>Get in touch</h6>
-                  <p class="touch-text">General contact info</p>
-                  <p class="touch-text">Licenses and Patents</p>
-                  <p class="touch-text">Purchasing and Logistic</p>
+                  <p className="touch-text">General contact info</p>
+                  <p className="touch-text">Licenses and Patents</p>
+                  <p className="touch-text">Purchasing and Logistic</p>
                 </div>
               </div>
-              <div class="col">
-                <div class="bosch">
+              <div className="col">
+                <div className="bosch">
                   <h6>You are on </h6>
                   <h4>Bosch Global</h4>
-                  <h5><a class="bosch-link" target="_blank" href="https://www.bosch.com/websites-worldwide/">Bosch worldwide</a></h5>
+                  <h5><a className="bosch-link" target="_blank" href="https://www.bosch.com/websites-worldwide/">Bosch worldwide</a></h5>
                   <h6>All Bosch apps</h6>
-                  <h5><a class="bosch-link" target="_blank" href="https://appcenter.bosch.com/">Bosch App Center</a></h5>
+                  <h5><a className="bosch-link" target="_blank" href="https://appcenter.bosch.com/">Bosch App Center</a></h5>
                 </div>
               </div>
             </div>
           </div>
-          <div class="container">
-          <div class="row bottom-text">
-          <div class="col-12 no-padding">
-            <span class="strong">&copy; 2018 Robert Bosch GmbH</span>
+          <div className="container">
+          <div className="row bottom-text">
+          <div className="col-12 no-padding">
+            <span className="strong">&copy; 2018 Robert Bosch GmbH</span>
             <span>Terms of use</span>
             <span>Legal notice</span>
             <span>Privacy statement</span>
@@ -191,7 +191,7 @@ class App extends Component {
             </div>
             </div>
           </div>
-          <img class="img-fluid" src={colores} alt="" />
+          <img className="img-fluid" src={colores} alt="" />
         </footer>
       </div>
       
